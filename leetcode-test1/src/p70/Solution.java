d2a0e8d314b7a3c2cb0b2572f500590806a83297
ZhangGC <43680528+GloryC@users.noreply.github.com>
2020-06-13 12:19:13 +0800

    p70

--- leetcode-test1/src/p70/Solution.java
+++ leetcode-test1/src/p70/Solution.java
@@ -6,36 +6,20 @@ package p70;
  */
 public class Solution {
 
-//    private int digui(int i, int n) {
-//        if (i > n) {
-//            return 0;
-//        }
-//        if (i == n) {
-//            return 1;
-//        }
-//        return digui(i + 1, n) + digui(i + 2, n);
-//    }
-//
-//    public int climbStairs(int n) {
-//        return digui(0, n);
-//    }
-
     public int climbStairs(int n) {
-        if (n == 1) return 1;
+        if (n <= 1) return 1;
         int[] dp = new int[n + 1];
         dp[1] = 1;
         dp[2] = 2;
         for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            dp[i] = dp[i - 2] + dp[i - 1];
         }
         return dp[n];
     }
 
-
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.climbStairs(6));
-
+        System.out.println(solution.climbStairs(3));
     }
 
 }
