96e4e64b5c655f459fdab68ad58ec8f3813aacc9
ZhangGC <43680528+GloryC@users.noreply.github.com>
2020-06-18 17:35:08 +0800

    p887

--- leetcode-test1/src/p887/Solution.java
+++ leetcode-test1/src/p887/Solution.java
@@ -1,55 +1,35 @@
 package p887;
 
-import java.util.Arrays;
+import java.util.HashMap;
 
 /**
- * 你将获得 K 个鸡蛋，并可以使用一栋从 1 到 N  共有 N 层楼的建筑。
- *
  * @author Glory
  * @date 2020/4/11 18:45
  */
 public class Solution {
+
+    private HashMap<String, Integer> memo = new HashMap<>();
+
     public int superEggDrop(int K, int N) {
+        return dp(K, N);
+    }
 
-        // 1、i 表示的是楼层的大小，不是第几层的意思，例如楼层区间 [8, 9, 10] 的大小为 3，这一点是在状态转移的过程中调整的定义
-        // 2、j 表示可以使用的鸡蛋的个数，它是约束条件，我个人习惯放在后面的维度，表示消除后效性的意思
-
-        // 0 个楼层和 0 个鸡蛋的情况都需要算上去，虽然没有实际的意义，但是作为递推的起点，被其它状态值所参考
-        int[][] dp = new int[N + 1][K + 1];
-        for (int i = 0; i <= N; i++) {
-            Arrays.fill(dp[i], i);
+    private int dp(int K, int N) {
+        if (K == 1) return N;
+        if (N == 0) return 0;
+        if (memo.containsKey(K + "+" + N)) {
+            return memo.get(K + "+" + N);
         }
-
-        for (int i = 0; i <= K; i++) {
-            dp[0][i] = 0;
+        int res = Integer.MAX_VALUE;
+        for (int i = 1; i <= N; i++) {
+            res = Math.min(res, Math.max(dp(K, N - i), dp(K - 1, i - 1)) + 1);
         }
-
-        dp[1][0] = 0;
-        for (int j = 1; j <= K; j++) {
-            dp[1][j] = 1;
-        }
-
-        // 第 0 列：鸡蛋个数为 0 的时候，不管楼层为多少，也测试不出鸡蛋的 F 值，故全为 0
-        // 第 1 列：鸡蛋个数为 1 的时候，这是一种极端情况，要试出 F 值，最少次数就等于楼层高度（想想复杂度的定义）
-        for (int i = 0; i <= N; i++) {
-            dp[i][0] = 0;
-            dp[i][1] = i;
-        }
-
-        for (int i = 2; i <= N ; i++) {
-            for (int j = 2; j <= K ; j++) {
-                for (int k = 1; k <= i; k++) {
-                    dp[i][j] = Math.min(dp[i][j], Math.max(dp[k - 1][j - 1], dp[i - k][j]) + 1);
-                }
-
-            }
-        }
-
-        return dp[N][K];
+        memo.put(K + "+" + N, res);
+        return res;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.superEggDrop(3, 14));
+        System.out.println(solution.superEggDrop(2, 6));
     }
 }
--- leetcode-test1/src/p887/Solution2.java
+++ leetcode-test1/src/p887/Solution2.java
@@ -1,44 +1,25 @@
 package p887;
 
-import java.util.Arrays;
-
 /**
- * 你将获得 K 个鸡蛋，并可以使用一栋从 1 到 N  共有 N 层楼的建筑。
- *
  * @author Glory
  * @date 2020/4/11 18:45
  */
 public class Solution2 {
+
     public int superEggDrop(int K, int N) {
         int[][] dp = new int[K + 1][N + 1];
-        for (int n = 1; n <= N; n++) {
-            dp[1][n] = n;
-        }
-        for (int k = 2; k <= K; k++) {
-            for (int n = 1; n <= N; n++) {
-                // dp[k][n] 表示k个鸡蛋、n个楼层时的最小移动次数。
-                // 即 dp[k][n] = min(1 + Math.max(dp[k][n - i], dp[k - 1][i - 1])); 1 <= i <= n。
-                // i 增大，dp[k][n - i]减小，dp[k - 1][i - 1]增大。
-                // 于是找到第 1 个 i 使得 dp[k][n - i] <= dp[k - 1][i - 1]。
-                int lo = 1, hi = n, mid = 0;
-                while (lo + 1 < hi) {
-                    mid = (lo + hi) >> 1;
-                    if (dp[k][n - mid] < dp[k - 1][mid - 1]) {
-                        hi = mid;
-                    } else if (dp[k][n - mid] > dp[k - 1][mid - 1]) {
-                        lo = mid;
-                    } else {
-                        lo = hi = mid;
-                    }
-                }
-                dp[k][n] = 1 + dp[k][n - lo];
+        int m = 0;
+        while (dp[K][m] < N) {
+            m++;
+            for (int k = 1; k <= K; k++) {
+                dp[k][m] = dp[k][m - 1] + dp[k - 1][m - 1] + 1;
             }
         }
-        return dp[K][N];
+        return m;
     }
 
     public static void main(String[] args) {
         Solution2 solution = new Solution2();
-        System.out.println(solution.superEggDrop(3, 14));
+        System.out.println(solution.superEggDrop(2, 6));
     }
 }
