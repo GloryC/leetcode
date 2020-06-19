fbbb3046a0f5b06ef1e8aab9de60fc7ce5970fc8
ZhangGC <43680528+GloryC@users.noreply.github.com>
2020-06-19 16:51:30 +0800

    p53

--- leetcode-test1/src/p53/Solution.java
+++ leetcode-test1/src/p53/Solution.java
@@ -5,26 +5,23 @@ package p53;
  * @date 2019/10/24 16:58
  */
 public class Solution {
-
     public int maxSubArray(int[] nums) {
-        int result = nums[0];
+        int res = nums[0];
         int sum = 0;
         for (int num : nums) {
-            if (sum > 0) {
+            if (sum > 0)
                 sum += num;
-            } else {
+            else
                 sum = num;
-            }
-            result = Math.max(result, sum);
+            res = Math.max(res, sum);
         }
-        return result;
+        return res;
     }
 
     public static void main(String[] args) {
-        int[] data = {};
+        int[] data = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
         Solution solution = new Solution();
         System.out.println(solution.maxSubArray(data));
     }
-
 
 }
