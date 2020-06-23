c2478113ad3b05d66be42336a1d31b9f39da602f
ZhangGC <43680528+GloryC@users.noreply.github.com>
2020-06-23 09:57:11 +0800

    p714

--- leetcode-test1/src/p714/Solution.java
+++ leetcode-test1/src/p714/Solution.java
@@ -6,21 +6,13 @@ package p714;
  */
 public class Solution {
     public int maxProfit(int[] prices, int fee) {
-        if (prices == null || prices.length == 0) return 0;
-        int[][] dp = new int[prices.length][2];
-        dp[0][0] = 0;
-        dp[0][1] = -prices[0];
-        for (int i = 1; i < prices.length; i++) {
-            // 第i天不持有股票的情况有两种
-            // a.第i - 1天也不持有股票
-            // b.第i天卖出
-            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee);
-            // 第i天持有股票有两种情况
-            // a.第i - 1天也持有股票
-            // b.第i - 1天不持有股票，在第i天买入(手续费算在卖出)
-            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        int dp_i_0 = 0, dp_i_1 = Integer.MIN_VALUE;
+        for (int price : prices) {
+            int temp = dp_i_0;  // i-1时的状态
+            dp_i_0 = Math.max(dp_i_0, dp_i_1 + price);
+            dp_i_1 = Math.max(dp_i_1, temp - price - fee);
         }
-        return dp[prices.length - 1][0];
+        return dp_i_0;
     }
 
     public static void main(String[] args) {
