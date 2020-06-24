7eaa446cca5a611501ec94c8aa7337e00c6dc3c9
ZhangGC <43680528+GloryC@users.noreply.github.com>
2020-06-24 09:08:07 +0800

    p5

--- leetcode-test1/src/p5/Solution.java
+++ leetcode-test1/src/p5/Solution.java
@@ -6,31 +6,48 @@ package p5;
  */
 public class Solution {
     public String longestPalindrome(String s) {
-        if (s == null || s.length() < 2) {
-            return s;
+        char[] data = s.toCharArray();
+        int len = data.length;
+        if (len < 2) return s;
+        int maxLen = 1, begin = 0;
+
+        // dp[i][j] 表示s[i]...s[j]是否为回文字符串
+        // P(i,j)=P(i+1,j−1)∧(Si==Sj)
+        boolean[][] dp = new boolean[len][len];
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
         }
-        int strLen = s.length();
-        int maxStart = 0;  //最长回文串的起点
-        int maxEnd = 0;    //最长回文串的终点
-        int maxLen = 1;  //最长回文串的长度
 
-        boolean[][] dp = new boolean[strLen][strLen];
-
-        for (int r = 1; r < strLen; r++) {
-            for (int l = 0; l < r; l++) {
-                if (s.charAt(l) == s.charAt(r) && (r - l <= 2 || dp[l + 1][r - 1])) {
-                    dp[l][r] = true;
-                    if (r - l + 1 > maxLen) {
-                        maxLen = r - l + 1;
-                        maxStart = l;
-                        maxEnd = r;
-
+        // 状态转移
+        // 注意：先填左下角
+        // 填表规则：先一列一列的填写，再一行一行的填，保证左下方的单元格先进行计算
+        for (int j = 1; j < len; j++) {
+            for (int i = 0; i < j; i++) {
+                // 头尾字符不相等，不是回文串
+                if (data[i] != data[j]) {
+                    dp[i][j] = false;
+                } else {
+                    // 相等的情况下
+                    // 考虑头尾去掉以后没有字符剩余，或者剩下一个字符的时候，肯定是回文串
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        // 状态转移
+                        dp[i][j] = dp[i + 1][j - 1];
                     }
                 }
-
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1; // 回文串从哪里结束
+                    begin = i; // 回文串从哪里开始
+                }
             }
-
         }
-        return s.substring(maxStart, maxEnd + 1);
+        return s.substring(begin, begin + maxLen);
     }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.longestPalindrome("bbab"));
+    }
+
 }
