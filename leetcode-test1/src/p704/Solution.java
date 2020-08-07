3150c74b6ba3cec9e35e96f9608831a5008520dd
ZhangGC <43680528+GloryC@users.noreply.github.com>
2020-08-07 15:38:26 +0800

    p704

---
+++ leetcode-test1/src/p704/Solution.java
@@ -0,0 +1,24 @@
+package p704;
+
+/**
+ * @author zhanggc
+ * @date 2020/8/7 15:32
+ */
+public class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) left = mid + 1;
+            else if (nums[mid] > target) right = mid - 1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        Solution solution = new Solution();
+        System.out.println(solution.search(nums, 2));
+    }
+}
