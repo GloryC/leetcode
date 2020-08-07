39b36c53a6c7257188c0f9e18adede54743e6360
ZhangGC <43680528+GloryC@users.noreply.github.com>
2020-08-07 08:56:18 +0800

    p100

---
+++ leetcode-test1/src/p100/Solution.java
@@ -0,0 +1,17 @@
+package p100;
+
+import util.TreeNode;
+
+/**
+ * @author zhanggc
+ * @date 2020/8/7 8:49
+ */
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+}
