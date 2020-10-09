2129c3cce47818693efcb2556427e56a9e63380f
ZhangGC <43680528+GloryC@users.noreply.github.com>
2020-10-09 17:02:10 +0800

    p26,p27,p83,p283

---
+++ leetcode-test1/src/p83/Solution2.java
@@ -0,0 +1,25 @@
+package p83;
+
+import util.ListNode;
+
+/**
+ * @author Glory
+ * @date 2020/3/24 18:09
+ */
+public class Solution2 {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) return null;
+        ListNode slow = head, fast = head;
+        while (fast != null) {
+            if (fast.val != slow.val) {
+                slow.next = fast;
+                slow = slow.next;
+            }
+            fast = fast.next;
+        }
+        slow.next = null;
+        return head;
+    }
+
+}
