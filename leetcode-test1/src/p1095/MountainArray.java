81bd6cc8079629faaf3fe25020aa78382658d90c
ZhangGC <764310647@qq.com>
2021-02-20 13:48:30 +0800

    change directory

--- leetcode-test1/src/p1095/MountainArray.java
+++
@@ -1,9 +0,0 @@
-package p1095;
-
-public interface MountainArray {
-
-    int get(int index);
-
-    int length();
-
-}
--- leetcode-test1/src/p327/Solution.java
+++
@@ -1,9 +0,0 @@
-package p327;
-
-/**
- * @author zhanggc
- * @date 2020/11/7 16:47
- */
-public class Solution {
-
-}
