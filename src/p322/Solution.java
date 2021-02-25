61d98f79a4aa726c1bc990762142f231737f3a22
ZhangGC <764310647@qq.com>
2021-02-25 14:37:13 +0800

    fix: p322

--- src/p322/Solution.java
+++ src/p322/Solution.java
@@ -8,26 +8,27 @@ import java.util.Arrays;
  */
 public class Solution {
 
+    /**
+     * 给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。
+     * 如果没有任何一种硬币组合能组成总金额，返回 -1。
+     */
     public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount + 1];
-        Arrays.fill(dp, amount + 1);
+        int max = amount + 1;
+        int[] dp = new int[max];
+        Arrays.fill(dp, max);
         dp[0] = 0;
-        for (int i = 0; i < dp.length; i++) {
+        for (int i = 1; i <= amount; i++) {
             for (int coin : coins) {
-                if (i < coin) continue;
-                dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
+                if (i >= coin) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
             }
         }
-
-        if (dp[amount] == amount + 1) {
-            return -1;
-        } else {
-            return dp[amount];
-        }
+        return (dp[amount] == max) ? -1 : dp[amount];
     }
 
     public static void main(String[] args) {
-        int[] coins = {2};
+        int[] coins = {1, 2};
         Solution solution = new Solution();
         System.out.println(solution.coinChange(coins, 3));
     }
