87ef268b3262e84cbde10ea1df4c12595f3d396c
ZhangGC <764310647@qq.com>
2021-03-02 15:59:07 +0800

    p701

---
+++ src/p701/Solution.java
@@ -0,0 +1,28 @@
+package p701;
+
+import util.TreeNode;
+
+/**
+ * @author zhanggc
+ * @date 2021/3/2 15:56
+ */
+public class Solution {
+
+    /**
+     * 给定二叉搜索树（BST）的根节点和要插入树中的值，将值插入二叉搜索树。 返回插入后二叉搜索树的根节点。
+     * 输入数据 保证 ，新值和原始二叉搜索树中的任意节点值都不同。
+     */
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+
+        if (root.val < val) {
+            root.right = insertIntoBST(root.right, val);
+        } else {
+            root.left = insertIntoBST(root.left, val);
+        }
+        return root;
+    }
+
+}
