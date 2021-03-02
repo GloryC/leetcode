637c036ad8addd70fce95ed534b3b6ae34d01151
ZhangGC <764310647@qq.com>
2021-03-02 11:39:43 +0800

    p304

---
+++ src/p304/NumMatrix.java
@@ -0,0 +1,37 @@
+package p304;
+
+/**
+ * @author zhanggc
+ * @date 2021/3/2 11:29
+ */
+public class NumMatrix {
+
+    private int[][] sums;
+
+    public NumMatrix(int[][] matrix) {
+        int m = matrix.length;
+        if (m > 0) {
+            int n = matrix[0].length;
+            sums = new int[m][n + 1];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    sums[i][j + 1] = sums[i][j] + matrix[i][j];
+                }
+            }
+        }
+    }
+
+    /**
+     * 给定一个二维矩阵，计算其子矩形范围内元素的总和，该子矩阵的左上角为 (row1, col1) ，右下角为 (row2, col2) 。
+     * 一维前缀和
+     */
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int sum = 0;
+        for (int i = row1; i <= row2; i++) {
+            sum += sums[i][col2 + 1] - sums[i][col1];
+        }
+
+        return sum;
+    }
+
+}
---
+++ src/p304/NumMatrix2.java
@@ -0,0 +1,32 @@
+package p304;
+
+/**
+ * @author zhanggc
+ * @date 2021/3/2 11:29
+ */
+public class NumMatrix2 {
+
+    private int[][] sums;
+
+    public NumMatrix2(int[][] matrix) {
+        int m = matrix.length;
+        if (m > 0) {
+            int n = matrix[0].length;
+            sums = new int[m + 1][n + 1];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    sums[i + 1][j + 1] = sums[i][j + 1] + sums[i + 1][j] - sums[i][j] + matrix[i][j];
+                }
+            }
+        }
+    }
+
+    /**
+     * 给定一个二维矩阵，计算其子矩形范围内元素的总和，该子矩阵的左上角为 (row1, col1) ，右下角为 (row2, col2) 。
+     * 一维前缀和
+     */
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return sums[row2 + 1][col2 + 1] - sums[row1][col2 + 1] - sums[row2 + 1][col1] + sums[row1][col1];
+    }
+
+}
