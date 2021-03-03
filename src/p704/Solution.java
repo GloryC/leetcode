e34ed9abe7fdd413e9cafe6307b7e26442085cfa
ZhangGC <764310647@qq.com>
2021-03-03 14:49:05 +0800

    fix: p704

--- src/p704/Solution.java
+++ src/p704/Solution.java
@@ -5,13 +5,22 @@ package p704;
  * @date 2020/8/7 15:32
  */
 public class Solution {
+
+    /**
+     * 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，
+     * 写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+     */
     public int search(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] == target) return mid;
-            else if (nums[mid] < target) left = mid + 1;
-            else if (nums[mid] > target) right = mid - 1;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] == target) {
+                return mid;
+            }
         }
         return -1;
     }
@@ -19,6 +28,6 @@ public class Solution {
     public static void main(String[] args) {
         int[] nums = {-1, 0, 3, 5, 9, 12};
         Solution solution = new Solution();
-        System.out.println(solution.search(nums, 2));
+        System.out.println(solution.search(nums, 9));
     }
 }
