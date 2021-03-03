3b67afa2cf70d76b95b1997773688dc710765696
ZhangGC <764310647@qq.com>
2021-03-03 14:58:12 +0800

    fix: p69

--- src/p69/Solution.java
+++ src/p69/Solution.java
@@ -5,18 +5,34 @@ package p69;
  * @date 2020/5/9 16:20
  */
 public class Solution {
+
+    /**
+     * 计算并返回 x 的平方根，其中 x 是非负整数。
+     * <p>
+     * 由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
+     *
+     * 采用二分查找法实现
+     */
     public int mySqrt(int x) {
-        long res = 0, i;
-        for (i = 1; res < x; i++) {
-            res = i * i;
+        int min = 1, max = x, mid = 0;
+        while (min <= max) {
+            mid = min + (max - min) / 2;
+            long value = (long) mid * mid;
+            if (value < x) {
+                min = mid + 1;
+            } else if (value > x) {
+                max = mid - 1;
+            } else if (value == x) {
+                return mid;
+            }
         }
-        i--;
-        return (int) (i * i == x ? i : i - 1);
+
+        return (long) mid * mid <= x ? mid : mid - 1;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.mySqrt(2147483647));
+        System.out.println(solution.mySqrt(8));
     }
 
 }
