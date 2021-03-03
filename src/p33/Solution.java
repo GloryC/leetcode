8810656517148f0f7d38d2dbca50ca92a3bf561d
ZhangGC <764310647@qq.com>
2021-03-03 15:24:18 +0800

    fix: p33

--- src/p33/Solution.java
+++ src/p33/Solution.java
@@ -6,22 +6,45 @@ package p33;
  */
 public class Solution {
 
+    /**
+     * 整数数组 nums 按升序排列，数组中的值 互不相同 。
+     * <p>
+     * 在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，使数组变为
+     * [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始
+     * 计数）。例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为 [4,5,6,7,0,1,2] 。
+     * <p>
+     * 给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的索引，否则返回 -1 
+     */
     public int search(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length - 1;
-        while (low < high) {
-            int mid = (low + high) / 2;
-            if (nums[0] <= nums[mid] && (target > nums[mid] || target < nums[0])) {
-                low = mid + 1;
-            } else if (target > nums[mid] && target < nums[0]) {
-                low = mid + 1;
+        int len = nums.length, left = 0, right = len - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < nums[right]) {
+                // 右半段有序
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             } else {
-                high = mid;
+                // 左半段有序
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
             }
         }
 
-        return low == high && nums[low] == target ? low : -1;
+        return -1;
+    }
 
+    public static void main(String[] args) {
+        int[] data = {4, 5, 6, 7, 0, 1, 2};
+        Solution solution2 = new Solution();
+        System.out.println(solution2.search(data, 0));
     }
 
 }
