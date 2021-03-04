d4f7312f66f7b296412f844a980532493e5f399e
ZhangGC <764310647@qq.com>
2021-03-04 14:17:16 +0800

    p278

---
+++ src/p278/Solution.java
@@ -0,0 +1,30 @@
+package p278;
+
+/**
+ * @author zhanggc
+ * @date 2021/3/4 14:06
+ */
+public class Solution {
+
+    /**
+     * 假设你有 n 个版本 [1, 2, ..., n]，你想找出导致之后所有版本出错的第一个错误的版本。
+     * 找左边界
+     */
+    public int firstBadVersion(int n) {
+        int left = 1, right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    private boolean isBadVersion(int version) {
+        return version % 2 == 1;
+    }
+
+}
