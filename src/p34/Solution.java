7f5b7980c570e5b04796033a388358a56144e9fd
ZhangGC <764310647@qq.com>
2021-03-04 15:03:59 +0800

    p34

--- src/p34/Solution.java
+++ src/p34/Solution.java
@@ -7,50 +7,40 @@ import java.util.Arrays;
  * @date 2020/8/7 15:38
  */
 public class Solution {
+
+    /**
+     * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+     * <p>
+     * 如果数组中不存在目标值 target，返回 [-1, -1]。
+     */
     public int[] searchRange(int[] nums, int target) {
-        int left = findLeft(nums, target);
-        int right = findRight(nums, target);
-        return new int[]{left, right};
+        int leftIdx = binarySearch(nums, target, true);
+        int rightIdx = binarySearch(nums, target, false) - 1;
+        if (leftIdx <= rightIdx && rightIdx < nums.length && nums[leftIdx] == target && nums[rightIdx] == target) {
+            return new int[]{leftIdx, rightIdx};
+        }
+
+        return new int[]{-1, -1};
     }
 
-    private int findLeft(int[] nums, int target) {
-        if (nums.length == 0) return -1;
-        int left = 0, right = nums.length; // 注意
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] == target) right = mid;
-            else if (nums[mid] < target) left = mid + 1;
-                // 因为我们的「搜索区间」是 [left, right) 左闭右开，所以当 nums[mid] 被检测之后，
-                // 下一步的搜索区间应该去掉 mid 分割成两个区间，即 [left, mid) 或 [mid + 1, right)
-            else if (nums[mid] > target) right = mid; // 注意
+    private int binarySearch(int[] nums, int target, boolean lower) {
+        int left = 0, right = nums.length - 1, ans = nums.length;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > target || (lower && nums[mid] >= target)) {
+                right = mid - 1;
+                ans = mid;
+            } else {
+                left = mid + 1;
+            }
         }
-        if (left == nums.length) return -1;
-        return nums[left] == target ? left : -1;
-    }
-
-    private int findRight(int[] nums, int target) {
-        if (nums.length == 0) return -1;
-        int left = 0, right = nums.length; // 注意
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] == target) left = mid + 1;
-            else if (nums[mid] < target) left = mid + 1;
-                // 因为我们的「搜索区间」是 [left, right) 左闭右开，所以当 nums[mid] 被检测之后，
-                // 下一步的搜索区间应该去掉 mid 分割成两个区间，即 [left, mid) 或 [mid + 1, right)
-            else if (nums[mid] > target) right = mid; // 注意
-        }
-        if (left == 0) return -1;
-        // 因为我们对 left 的更新必须是 left = mid + 1，就是说 while 循环结束时，
-        // nums[left] 一定不等于 target 了，而 nums[left-1] 可能是 target。
-        // 因为收紧左侧边界时必须 left = mid + 1
-        // 所以最后无论返回 left 还是 right，必须减一
-        return nums[left - 1] == target ? (left - 1) : -1;
+        return ans;
     }
 
     public static void main(String[] args) {
         int[] nums = {5, 7, 7, 8, 8, 10};
         Solution solution = new Solution();
-        System.out.println(Arrays.toString(solution.searchRange(nums, 5)));
+        System.out.println(Arrays.toString(solution.searchRange(nums, 8)));
     }
 
 }
