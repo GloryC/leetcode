eac44600b694be28c112add16d420e0bd1c62f91
ZhangGC <764310647@qq.com>
2021-03-04 14:50:52 +0800

    p153

--- src/p153/Solution.java
+++ src/p153/Solution.java
@@ -6,34 +6,28 @@ package p153;
  */
 public class Solution {
 
+    /**
+     * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] 。
+     * <p>
+     * 请找出其中最小的元素。
+     */
     public int findMin(int[] nums) {
-        if (nums.length == 1) {
-            return nums[0];
-        }
-
         int left = 0, right = nums.length - 1;
-
-        if (nums[right] > nums[0]) {
-            return nums[0];
-        }
-
-        while (right >= left) {
-            int mid = (right + left) / 2;
-            if (nums[mid] > nums[mid + 1]) {
-                return nums[mid + 1];
-            }
-
-            if (nums[mid - 1] > nums[mid]) {
-                return nums[mid];
-            }
-
-            if (nums[mid] > nums[0]) {
-                left = mid + 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[right]) {
+                right = mid;
             } else {
-                right = mid - 1;
+                left = mid + 1;
             }
         }
-        return -1;
+
+        return nums[left];
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.findMin(new int[]{4, 5, 6, 7, 0, 1, 2}));
     }
 
 }
