b88feb202209075e43e72fcae7a18f4d38b01006
ZhangGC <764310647@qq.com>
2021-05-19 15:45:31 +0800

    p1738

--- src/com/glory/leetcode/p1738/FindKthLargestXorCoordinateValue.java
+++ src/com/glory/leetcode/p1738/FindKthLargestXorCoordinateValue.java
@@ -48,8 +48,6 @@
 package com.glory.leetcode.p1738;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -62,27 +60,23 @@ public class FindKthLargestXorCoordinateValue {
         public int kthLargestValue(int[][] matrix, int k) {
             int m = matrix.length, n = matrix[0].length;
             int[][] pre = new int[m + 1][n + 1];
-            List<Integer> results = new ArrayList<Integer>();
+            List<Integer> results = new ArrayList<>();
             for (int i = 1; i <= m; ++i) {
                 for (int j = 1; j <= n; ++j) {
+                    // 前缀和
                     pre[i][j] = pre[i - 1][j] ^ pre[i][j - 1] ^ pre[i - 1][j - 1] ^ matrix[i - 1][j - 1];
                     results.add(pre[i][j]);
                 }
             }
 
-            Collections.sort(results, new Comparator<Integer>() {
-                public int compare(Integer num1, Integer num2) {
-                    return num2 - num1;
-                }
-            });
+            results.sort((num1, num2) -> num2 - num1);
             return results.get(k - 1);
-
-
         }
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
+        System.out.println(solution.kthLargestValue(new int[][]{{5, 2}, {1, 6}}, 1));
     }
 
 }
