d25dbff70535a232582d3552f2b1584d9ee32ab5
ZhangGC <43680528+GloryC@users.noreply.github.com>
2022-01-10 21:33:00 +0800

    p2

---
+++ src/com/glory/leetcode/p2/AddTwoNumbers.java
@@ -0,0 +1,42 @@
+package com.glory.leetcode.p2;
+
+import util.ListNode;
+
+/**
+ * @author Glory
+ * @date 2020/6/30 9:07
+ */
+public class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        // 在两条链表上的指针
+        ListNode p1 = l1, p2 = l2;
+        // 构建虚拟头结点
+        ListNode dummy = new ListNode(-1);
+        // 指针p负责构建新链表
+        ListNode p = dummy;
+        // 记录进位
+        int carry = 0;
+        // 开始执行加法，两条链表走完且没有进位时才能结束循环
+        while (p1 != null || p2 != null || carry > 0) {
+            // 先加上上次的进位
+            int val = carry;
+            if (p1 != null) {
+                val += p1.val;
+                p1 = p1.next;
+            }
+            if (p2 != null) {
+                val += p2.val;
+                p2 = p2.next;
+            }
+            // 处理进位情况
+            carry = val / 10;
+            val = val % 10;
+            // 构建新节点
+            p.next = new ListNode(val);
+            p = p.next;
+        }
+        // 返回结果链表的头结点（去除虚拟头结点）
+        return dummy.next;
+    }
+
+}
